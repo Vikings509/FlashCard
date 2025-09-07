add7a781bec236f501e2939762f0d940fa222463
Vikings509 <exinard@gmail.com>
2025-09-07 11:40:52 -0400

    Step 7

--- app/src/main/java/com/example/flashcard/MainActivity.kt
+++ app/src/main/java/com/example/flashcard/MainActivity.kt
@@ -2,46 +2,76 @@ package com.example.flashcard
 
 import android.os.Bundle
 import android.view.View
+import android.view.animation.RotateAnimation
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.R.transition.fade
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import kotlin.time.toDuration
 
 class MainActivity : AppCompatActivity() {
+    private var isOn = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        val flashcardQuestion =  findViewById<TextView>(R.id.flashcard_question1)
-        val flashcardAnswer =  findViewById<TextView>(R.id.flashcard_answer1)
-        flashcardQuestion.setOnClickListener {
-            flashcardQuestion.visibility = View.INVISIBLE
-            flashcardAnswer.visibility = View.VISIBLE
+        // Set title to FlashCard App
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        supportActionBar?.title = "FlashCard App"
 
-        }
+        // Get the question and answer text views
+        val flashcardQuestion = findViewById<TextView>(R.id.flashcard_question1)
+        val flashcardAnswer = findViewById<TextView>(R.id.flashcard_answer1)
+            flashcardQuestion.setOnClickListener {
+                flashcardQuestion.visibility = View.INVISIBLE
+                flashcardAnswer.visibility = View.VISIBLE
+            }
         flashcardAnswer.setOnClickListener {
             flashcardQuestion.visibility = View.VISIBLE
             flashcardAnswer.visibility = View.INVISIBLE
-             val isShowingAnswers =
-                 findViewById<ImageView>(R.id.toggle_choices_visibility)
+        }
+            // Set values
+            val isShowingAnswers = findViewById<ImageView>(R.id.toggle_choices_visibility)
             val flashcardChoice1 = findViewById<TextView>(R.id.flashcard_q1_choice1)
             val flashcardChoice2 = findViewById<TextView>(R.id.flashcard_q1_choice2)
             val flashcardChoice3 = findViewById<TextView>(R.id.flashcard_q1_choice3)
             // set initial icon
-            isShowingAnswers.setImageResource(R.drawable.eye_lined_off)
             isShowingAnswers.setOnClickListener {
-                flashcardChoice1.visibility = View.INVISIBLE
-                flashcardChoice2.visibility = View.INVISIBLE
-                flashcardChoice3.visibility = View.INVISIBLE
-            }
+                isOn = !isOn
 
-        }
+                // Animation rotation
+                val rotate = RotateAnimation(
+                    0f, 360f,
+                    Animation.RELATIVE_TO_SELF, 0.5f,
+                    Animation.RELATIVE_TO_SELF, 0.5f
+                )
+                rotate.duration = 300
+                isShowingAnswers.startAnimation(rotate)
+                AlphaAnimation(0f,1f)
+
+                 // Changer l'icone
+                if (isOn){
+                    isShowingAnswers.setImageResource(R.drawable.eye_lined)
+                    flashcardChoice1.visibility = View.INVISIBLE
+                    flashcardChoice2.visibility=View.INVISIBLE
+                    flashcardChoice3.visibility= View.INVISIBLE
+                }
+                    else {
+                    isShowingAnswers.setImageResource(R.drawable.eye_lined_off)
+                    flashcardChoice1.visibility = View.VISIBLE
+                    flashcardChoice2.visibility=View.VISIBLE
+                    flashcardChoice3.visibility= View.VISIBLE
+                            }
             ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
         }
     }
 }
